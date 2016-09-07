75618a7fc39abaada1ef3d67843e1d90b1357c24
Mason <maslopez@csumb.edu>
2016-09-06 22:27:24 -0700

    Updated test class test cases

--- test/cst438Assign_1_Lopez_Mason/QuestionsAnswersTest.java
+++ test/cst438Assign_1_Lopez_Mason/QuestionsAnswersTest.java
@@ -7,13 +7,15 @@ import org.junit.Test;
 public class QuestionsAnswersTest {
 
 	@Test
-	public void test() {
+	public void handlingWeirdInputsForTestAnswerMethod() {
 		//fail("Not yet implemented");
+		QuestionsAnswers implementQuestions = new QuestionsAnswers();
+		assertFalse(implementQuestions.testAnswer("42","Randomness here!"));
 	}
 	@Test 
 	public void main() {
 		QuestionsAnswers queAn = new QuestionsAnswers();
-		System.out.println(queAn.getRandomQuestion());
+		//System.out.println(queAn.getRandomQuestion());
 		//System.out.println("asdfasdfadfa");
 		
     }
